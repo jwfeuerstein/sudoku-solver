9da90f401a0a764eabbe2e54790cbee3c57dcee8
Jason Feuerstein <jwfeuers@usc.edu>
2020-06-25 17:00:05 -0700

    Update sudoku.h

--- sudoku.h
+++ sudoku.h
@@ -7,16 +7,15 @@ public:
 	Sudoku(int puzzle[9][9]);
 	~Sudoku();
 
-	void solve(); // solves the puzzle
-	void print(); // prints the puzzle
-	void verify(); // verifies the puzzle is valid
+	void solve(); 
+	void print(); 
+	void verify(); 
 
 private:
 	bool isValid(int row, int col);
 	bool solveHelper(int row, int col);
 	int board[9][9];
 
-	// add extra helper functions here
 };
 
 #endif
